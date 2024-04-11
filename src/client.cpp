ca28ee7b2ed27233b281bc32c4ded3f3ae05420b
Winfried Auner <18383180+wAuner@users.noreply.github.com>
2024-04-11 11:18:07 +0200

    basic message sending

---
+++ src/client.cpp
@@ -0,0 +1,73 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+
+#include <iostream>
+#include <print>
+#include <format>
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <port number>" << std::endl;
+        return 1;
+    }
+
+    addrinfo hints{.ai_protocol = IPPROTO_TCP,
+                   .ai_socktype = SOCK_STREAM,
+                   .ai_family = PF_INET};
+
+    addrinfo* result;
+
+    int getaddrresult = getaddrinfo("localhost", argv[1], &hints, &result);
+    if (getaddrresult != 0) {
+        std::cerr << "getaddrinfo failed: " << gai_strerror(getaddrresult) << std::endl;
+        return 1;
+    }
+
+    if (result == nullptr) {
+        std::cerr << "getaddrinfo returned nullptr" << std::endl;
+        return 1;
+    }
+
+    int sockFd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+
+    if (sockFd == -1) {
+        std::cerr << "socket failed" << std::endl;
+        return 1;
+    }
+
+    int connectResult = connect(sockFd, result->ai_addr, result->ai_addrlen);
+
+    if (connectResult == -1) {
+        std::cerr << "connect failed" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Connected to server" << std::endl;
+
+    while (true) {
+        std::string message;
+        std::println("Please enter message:");
+        std::getline(std::cin, message);
+
+        if (message == "exit") {
+            break;
+        }
+
+        int sendResult = send(sockFd, message.c_str(), message.size(), 0);
+
+        if (sendResult == -1) {
+            perror("send failed");
+            return 1;
+        }
+
+        std::cout << "Sent " << sendResult << " bytes" << std::endl;
+    }
+
+    std::println("Closing connection");
+    close(sockFd);
+    freeaddrinfo(result);
+
+
+    return 0;
+}
--- src/server.cpp
+++ src/server.cpp
@@ -1,47 +1,107 @@
+#include <fcntl.h>
+#include <netdb.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <netdb.h>
 
 #include <format>
-#include <print>
 #include <iostream>
+#include <print>
 
 using namespace std;
 
-const char* protocolToString(int protocol) {
-    switch (protocol) {
-        case IPPROTO_TCP:
-            return "TCP";
-        case IPPROTO_UDP:
-            return "UDP";
-        default:
-            return "Unknown";
+std::pair<string, string> getHostAndPort(const sockaddr *addr) {
+    char host[NI_MAXHOST];
+    char port[NI_MAXSERV];
+    // maybe sockaddr_storage is better? that's what copilot suggested
+    int getnameinforesult = getnameinfo(addr, sizeof(addr->sa_len), host,
+                                        NI_MAXHOST, port, NI_MAXSERV, 0);
+    if (getnameinforesult != 0) {
+        return {"<unknown>", "<unknown>"};
     }
+    return {host, port};
 }
 
-int main(int argc, const char * argv[]) {
-   addrinfo hints {.ai_protocol = IPPROTO_TCP};
-   addrinfo* result;
-   
-   int getaddrresult = getaddrinfo("vastlimits.com", "https", &hints, &result);
-   if (getaddrresult != 0) {
-      println("getaddrinfo failed: <{}>", gai_strerror(getaddrresult));
-      return -1;
-   }
-   
-   int counter = 1;
-   for (addrinfo* entry = result; entry != nullptr; entry = entry->ai_next) {
-      char address[NI_MAXHOST];
-      char service[NI_MAXSERV];
-      if (getnameinfo(entry->ai_addr, entry->ai_addrlen, address, NI_MAXHOST, service, NI_MAXSERV, NI_NUMERICHOST) != 0) {
-         println("getnameinfo failed: <{}>", gai_strerror(getaddrresult));
-         return -1;
-      
-      }
-      println("Element no. {}: address is: <{}>, service is: <{}>, protocol: <{}>", counter, address, service, protocolToString(entry->ai_protocol));
-      counter++;
-   }
-   
-   println("hello {}", "world");
-   return 0;
+int main() {
+    addrinfo hints{.ai_protocol = IPPROTO_TCP,
+                   .ai_socktype = SOCK_STREAM,
+                   .ai_family = PF_INET};
+    //    .ai_flags = AI_PASSIVE}; // with AI_PASSIVE, we can use nullptr for
+    //    the host and it binds to all interfaces
+    // The three members ai_family, ai_socktype, and
+    // ai_protocol in each returned addrinfo structure are suitable for a call
+    // to socket(2)
+    addrinfo *result;
+    const char *port = "50069";
+    int getaddrresult = getaddrinfo("localhost", port, &hints, &result);
+    if (getaddrresult != 0) {
+        println("getaddrinfo failed: <{}>", gai_strerror(getaddrresult));
+        return -1;
+    }
+
+    if (result == nullptr) {
+        println("getaddrinfo returned nullptr");
+        return -1;
+    }
+
+    // just use the first result
+    const auto [host, portString] = getHostAndPort(result->ai_addr);
+    int sockFd =
+        socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+
+    if (sockFd == -1) {
+        println("socket failed: <{}>", strerror(errno));
+        return -1;
+    }
+
+    if (bind(sockFd, result->ai_addr, result->ai_addrlen) == -1) {
+        println("bind failed: <{}>", strerror(errno));
+        return -1;
+    }
+    println("bound to port {}", portString);
+
+    if (listen(sockFd, 10) == -1) {
+        println("listen failed: <{}>", strerror(errno));
+        return -1;
+    }
+
+    println("listening for incoming connections on {}:{}", host, portString);
+
+    sockaddr_storage clientAddr;
+    socklen_t clientAddrLen = sizeof(clientAddr);
+    do {
+        int clientFd = accept(sockFd, (sockaddr *)&clientAddr, &clientAddrLen);
+        if (clientFd == -1) {
+            println("accept failed: <{}>", strerror(errno));
+            return -1;
+        }
+        const auto [clientHost, clientPort] =
+            getHostAndPort((sockaddr *)&clientAddr);
+        println("accepted connection from {}:{}", clientHost, clientPort);
+
+        std::vector<char> buffer(1024);
+        int bytesReceived;
+        while (true) {
+            bytesReceived = recv(clientFd, buffer.data(), buffer.size(), 0);
+            if (bytesReceived == -1) {
+                println("recv failed: <{}>", strerror(errno));
+                return -1;
+            } else if (bytesReceived > 0) {
+                std::string receivedData(buffer.data(), bytesReceived);
+                println("received {} bytes: <{}>", bytesReceived, receivedData);
+            } else if (bytesReceived == 0) {
+                println("client closed connection");
+                break;
+            }
+            buffer.clear();
+        } 
+
+        close(clientFd);
+        println("closed connection to {}:{}", clientHost, clientPort);
+    } while (true);
+    close(sockFd);
+
+    freeaddrinfo(result);
+
+    println("hello {}", "world");
+    return 0;
 }
